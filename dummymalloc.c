92d9240d6979726a2586475223e143aa32318830
NVanvyve <nicolas.vanvyve@hotmail.com>
2016-03-10 23:48:56 +0100

    Projet de merde

--- dummymalloc.c
+++ dummymalloc.c
@@ -1,19 +1,57 @@
-#include <assert.h>
-#include <string.h>
-#include <sys/types.h>
+#include <ctype.h>
 #include <unistd.h>
+#include <stdio.h>
+
+/*
+Trouver un moyen de stoker l'adresse du sommet du HEAP
+avant le début de la première excéction de mymalloc
+
+Cette variable est FLOOR
+
+*/
+
+
+
+typedef struct block_header {
+  unsigned int    sizeblock : 29,
+                  zero : 2,
+                  alloc: 1;
+} block_header;
 
 int main(int argc, char const *argv[]) {
   return 0;
 }
 
-void *malloc(size_t size) {
+void *mymalloc(size_t size)
+{
+  void *ptr = sbrk(0);
+  if (ptr== FLOOR)
+  {
+    void *p = sbrk(0);
+    void *request = sbrk(size);
+    if (request == (void*) -1)
+    {
+      return NULL; // sbrk failed.
+    }
+    else
+    {
+      return p;
+    }
+  }
+
+}
+
+/*
+void *dummymalloc(size_t size) {
   void *p = sbrk(0);
   void *request = sbrk(size);
-  if (request == (void*) -1) {
+  if (request == (void*) -1)
+  {
     return NULL; // sbrk failed.
-  } else {
-    assert(p == request); // Not thread safe.
+  }
+  else
+  {
     return p;
   }
 }
+*/
--- mymalloc.c
+++ mymalloc.c
@@ -2,21 +2,25 @@
 #include <ctype.h>
 #include <unistd.h>
 #define size4(x) (((((x)-1)>>2)<<2)+4)
-constant size_t BH_SIZE=sizeof(block_header)
-
-static block_header *floor = (block_header *) sbrk(0); //Test sans le const
 
 typedef struct block_header {
-  unsigned int    size : 29,
+  unsigned int    sizeblock : 29,
                   zero : 2,
                   alloc: 1;
 } block_header;
+
+const size_t BH_SIZE=sizeof(block_header);
+
+static block_header *floor;
+floor = (block_header *) sbrk(0);
 
 void *mymalloc (size_t size) {
   size = size4(size);
 
   block_header *limit = (block_header *) sbrk(0);
-  void *ptr = FLOOR;
+  //void *ptr = floor;
+  block_header *ptr;
+  ptr = *floor;
   size_t size_disp = 0;
 
   while (size_disp < size) {
@@ -25,7 +29,7 @@ void *mymalloc (size_t size) {
 		  void * err = sbrk(size+4);
 		  if (err == (void *) -1) return NULL;
 		  (*limit)->alloc = 1;
-		  (*limit)->size = size;
+		  (*limit)->sizeblock = size;
 		  return limit+BH_SIZE;
 	  }
 
@@ -34,24 +38,24 @@ void *mymalloc (size_t size) {
 		  void * err = sbrk(inc);
 		  if (err == (void *) -1) return NULL;
 		  *ptr->alloc = 1;
-		  *ptr->size = size;
+		  *ptr->sizeblock = size;
 		  return ptr+BH_SIZE;
 	  }
 
-	  if (*(ptr+size_disp)->alloc == 0) size_disp += *(ptr+size_disp)->size +BH_SIZE;
+	  if (*(ptr+size_disp)->alloc == 0) size_disp += *(ptr+size_disp)->sizeblock +BH_SIZE;
 
 	  if (*(ptr+size_disp)->alloc == 1) {
-		  ptr += *(ptr+size_disp)->size + size_disp;
+		  ptr += *(ptr+size_disp)->sizeblock + size_disp;
 		  size_disp = 0;
 	  }
   }
 
   if (size_disp != size) {
 	  *(ptr+size)->alloc = 0;
-	  *(ptr+size)->size = size_disp - size;
+	  *(ptr+size)->sizeblock = size_disp - size;
   }
 
   *ptr->alloc = 1;
-  *ptr->size = size;
+  *ptr->sizeblock = size;
   return ptr+BH_SIZE;
 }
