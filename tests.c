b18269bac4841c82d2ad04273a50e69c7b5360b3
NVanvyve <nicolas.vanvyve@hotmail.com>
2016-03-11 16:08:38 +0100

    Test de tests CUNIT

--- tests.c
+++ tests.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <unistd.h>
-#include <signal.h>
 
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
-//#include<mymalloc.h>
+//#include<mymalloc.h> //ou machin du genre
 
-/* TESTS POUR MALLOC */
+/* TESTS POUR MALLOC
 
-/*
   - Verifier que on renvoie bien void*
   - Allouer de la memoire pour deux entités
     différente et vérifier que il n'y a pas de réécriture
-  - 
+  -
 */
+
+/* TESTS POUR CALLOC
+
+  - Idem MALLOC
+  - Apres allocation est ce que la valeur
+    est bien egale a 0
+  -
+*/
+
+
+
+/* TESTS POUR FREE
+  - Euh...
+  -
+*/
+
+
+/* Exemples de tests */
+void test_assert_true(void)
+{
+  CU_ASSERT(true);
+}
+void test_assert_2_not_equal_minus_1(void)
+{
+  CU_ASSERT_NOT_EQUAL(2, -1);
+}
+void test_string_equals(void)
+{
+  CU_ASSERT_STRING_EQUAL("string #1", "string #1");
+}
+void test_failure(void)
+{
+  CU_ASSERT(false);
+}
+void test_string_equals_failure(void)
+{
+  CU_ASSERT_STRING_EQUAL("string #1", "string #2");
+}
+
+
+if (CUE_SUCCESS != CU_initialize_registry())
+{
+  return CU_get_error();
+}
+
+int main()
+{
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+  return CU_get_error();
+}
