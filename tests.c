3402520b06372286124b98cac29159bee486dced
NVanvyve <nicolas.vanvyve@hotmail.com>
2016-03-11 12:57:49 +0100

    Début des fichiers test

---
+++ tests.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
