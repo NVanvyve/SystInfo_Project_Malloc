d878b4ddf83272b01ea917eb975c19c278f22c84
NVanvyve <nicolas.vanvyve@hotmail.com>
2016-03-14 13:34:59 +0100

    Synchro Tests

--- tests.c
+++ tests.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
+#include <string.h>
+#include "CUnit/Basic.h"
 
 //#include<mymalloc.h> //ou machin du genre
 
@@ -31,36 +29,102 @@
 
 
 /* Exemples de tests */
-void test_assert_true(void)
-{
-  CU_ASSERT(true);
-}
-void test_assert_2_not_equal_minus_1(void)
-{
-  CU_ASSERT_NOT_EQUAL(2, -1);
-}
-void test_string_equals(void)
-{
-  CU_ASSERT_STRING_EQUAL("string #1", "string #1");
-}
-void test_failure(void)
-{
-  CU_ASSERT(false);
-}
-void test_string_equals_failure(void)
-{
-  CU_ASSERT_STRING_EQUAL("string #1", "string #2");
-}
 
 
-if (CUE_SUCCESS != CU_initialize_registry())
+/* Pointer to the file used by the tests. */
+static FILE* temp_file = NULL;
+
+/* The suite initialization function.
+ * Opens the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite1(void)
 {
-  return CU_get_error();
+   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
+      return -1;
+   }
+   else {
+      return 0;
+   }
 }
 
+/* The suite cleanup function.
+ * Closes the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite1(void)
+{
+   if (0 != fclose(temp_file)) {
+      return -1;
+   }
+   else {
+      temp_file = NULL;
+      return 0;
+   }
+}
+
+/* Simple test of fprintf().
+ * Writes test data to the temporary file and checks
+ * whether the expected number of bytes were written.
+ */
+void testFPRINTF(void)
+{
+   int i1 = 10;
+
+   if (NULL != temp_file) {
+      CU_ASSERT(0 == fprintf(temp_file, ""));
+      CU_ASSERT(2 == fprintf(temp_file, "Q\n"));
+      CU_ASSERT(7 == fprintf(temp_file, "i1 = %d", i1));
+   }
+}
+
+/* Simple test of fread().
+ * Reads the data previously written by testFPRINTF()
+ * and checks whether the expected characters are present.
+ * Must be run after testFPRINTF().
+ */
+void testFREAD(void)
+{
+   unsigned char buffer[20];
+
+   if (NULL != temp_file) {
+      rewind(temp_file);
+      CU_ASSERT(9 == fread(buffer, sizeof(unsigned char), 20, temp_file));
+      CU_ASSERT(0 == strncmp(buffer, "Q\ni1 = 10", 9));
+   }
+}
+
+/* The main() function for setting up and running the tests.
+ * Returns a CUE_SUCCESS on successful running, another
+ * CUnit error code on failure.
+ */
 int main()
 {
-  CU_basic_run_tests();
-  CU_cleanup_registry();
-  return CU_get_error();
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if ((NULL == CU_add_test(pSuite, "test of fprintf()", testFPRINTF)) ||
+       (NULL == CU_add_test(pSuite, "test of fread()", testFREAD)))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
 }
