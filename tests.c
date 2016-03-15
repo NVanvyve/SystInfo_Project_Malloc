fbfb212bca9123ea0f57dc97d674ea0bfc3e8efb
NVanvyve <nicolas.vanvyve@hotmail.com>
2016-03-15 23:41:06 +0100

    Petites modif pour la beauté de la chose... Au dodo

--- tests.c
+++ tests.c
@@ -101,26 +101,26 @@ int main()
    if (CUE_SUCCESS != CU_initialize_registry())
       return CU_get_error();
 
-   pSuite = CU_add_suite("Suite Projet", setup, teardown);
+   pSuite = CU_add_suite("Projet-Malloc", setup, teardown);
    if (NULL == pSuite) {
       CU_cleanup_registry();
       return CU_get_error();
    }
 
-   if ((NULL == CU_add_test(pSuite, "Test myfree desalloc",test_myfree_desalloc)) ||
-       (NULL == CU_add_test(pSuite, "Test mymalloc alloc",test_mymalloc_alloc)) ||
-       (NULL == CU_add_test(pSuite, "Test mycalloc alloc",test_mycalloc_alloc)) ||
-       (NULL == CU_add_test(pSuite, "Test mymalloc size",test_mymalloc_size)) ||
-       (NULL == CU_add_test(pSuite, "Test mymalloc NULL",test_mymalloc_NULL)) ||
-       (NULL == CU_add_test(pSuite, "Test mymalloc 2 alloc",test_mymalloc_two_alloc)) ||
-       (NULL == CU_add_test(pSuite, "Test mycalloc initialisation",test_mycalloc_init))
+   if ((NULL == CU_add_test(pSuite, "Myfree desalloc",test_myfree_desalloc)) ||
+       (NULL == CU_add_test(pSuite, "Mymalloc alloc",test_mymalloc_alloc)) ||
+       (NULL == CU_add_test(pSuite, "Mycalloc alloc",test_mycalloc_alloc)) ||
+       (NULL == CU_add_test(pSuite, "Mymalloc size",test_mymalloc_size)) ||
+       (NULL == CU_add_test(pSuite, "Mymalloc NULL",test_mymalloc_NULL)) ||
+       (NULL == CU_add_test(pSuite, "Mymalloc 2 alloc",test_mymalloc_two_alloc)) ||
+       (NULL == CU_add_test(pSuite, "Mycalloc initialisation",test_mycalloc_init))
      )
    {
      CU_cleanup_registry();
      return CU_get_error();
    }
 
-//   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
    printf("\n");
    CU_basic_show_failures(CU_get_failure_list());
