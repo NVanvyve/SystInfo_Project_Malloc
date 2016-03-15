b1355d68c3d5a3cba262fe9fc6842e3487155b5d
Julien Aigret <julien.aigret@student.uclouvain.be>
2016-03-15 12:54:54 +0100

    Best fit

--- mymalloc.c
+++ mymalloc.c
@@ -1,119 +1,115 @@
-/*
-Bonjour,
-
-Une petite précision importante concernant le premier projet.
-Dans l'énoncé, il est indiqué qu'il est possible d'étendre la taille du heap
-grâce à l'appel système sbrk().Pour le projet, nous vous demandons d'allouer une
-taille fixe, précisée à la ligne de commande, au début de votre
-programme (typiquement, lors du premier appel à mymalloc()). Cette extension ne
-doit être effectuée qu'une seule fois. Cela signifie que lorsque qu'il est
-impossible de satisfaire un appel à mymalloc avec l'espace préalloué
-(i.e. il n'y a pas assez d'espace libre), mymalloc doit retourner NULL
-et ne pas étendre le heap.
-
-Concrètement, le premier appel à mymalloc doit effectuer quelque chose comme cela:
-
-base_heap = sbrk(0);
-
-sbrk(memsize);
-
-où base_heap est un pointeur et memsize est la taille de la mémoire passé en paramètre.
-
-David
-*/
-
 #include <stdio.h>
 #include <ctype.h>
 #include <unistd.h>
 #define size4(x) (((((x)-1)>>2)<<2)+4)
-#define BH_SIZE (sizeof(block_header))
+#define BH_SIZE 4
 
 
-static const size_t INIT = 1024;
+static const size_t MB = 1024*1024;
 
 
 
 typedef struct block_header {
-  unsigned int    sizeblock : 29,
+  unsigned int    size : 29,
                   zero : 2,
                   alloc: 1;
 } block_header;
 
 static block_header *FLOOR = NULL;
 static block_header *limit = NULL;
+static block_header *last = NULL;
 
 void *mymalloc (size_t size_asked) {
+
+  if (size_asked == 0) return NULL;
+
   size_asked = size4(size_asked);
 
   if (FLOOR == NULL) {
     FLOOR = (block_header *) sbrk(0);
-    void * err = sbrk(INIT+BH_SIZE);
+    void * err = sbrk(MB+BH_SIZE);
     if (err == (void *) -1) return NULL;
     FLOOR->alloc = 0;
-    FLOOR->sizeblock = INIT-BH_SIZE;
-	limit = FLOOR + FLOOR->sizeblock;
-	limit->alloc = 0;
-	limit->sizeblock = 0;
+    FLOOR->size = MB;
+    limit = (block_header *) FLOOR + MB + BH_SIZE;
+    last = FLOOR;
   }
 
 
   block_header *ptr = FLOOR;
   size_t size_disp = 0;
+  int best_fit = 0;
+  int fit = 0;
+  int best_size = 0;
+  int limit_reached = 0;
+  block_header *best_ptr = NULL;
 
-  while (size_disp < size_asked) {
+  while (!best_fit && !limit_reached) {
+    size_disp = 0;
 
-    printf("BSIZE : %i, SIZE_DISP : %ld, SIZE_ASKED : %ld\n", ptr->sizeblock, size_disp, size_asked);
-    printf("Pointer : %p, LIMIT : %p\n", ptr, limit);
+    while ((ptr + size_disp) < last && (ptr+size_disp)->alloc == 0 && size_disp != size_asked){
+      size_disp += (ptr + size_disp)->size + BH_SIZE;
+    }
+    //printf("COUNT OUT\n");
 
-	  if (ptr == limit) {
-		  printf("LIMIT == PTR\n");
-		  void * err = sbrk(size_asked+BH_SIZE);
-		  if (err == (void *) -1) return NULL;
-		  ptr->alloc = 1;
-		  ptr->sizeblock = size_asked;
-		  limit += size_asked+BH_SIZE;
-		  limit->alloc = 0;
-		  limit->sizeblock = 0;
-		  return ptr+BH_SIZE;
-	  }
-
-	  if (ptr+size_disp == limit) {
-		  printf("Limit Reached\n");
-		  int inc = size_asked + BH_SIZE - size_disp;
-		  void * err = sbrk(inc);
-		  if (err == (void *) -1) return NULL;
-		  ptr->alloc = 1;
-		  ptr->sizeblock = size_asked;
-		  limit = ptr + size_asked;
-		  limit->alloc=0;
-		  limit->sizeblock = 0;
-		  return ptr+BH_SIZE;
-	  }
-
-	  if ((ptr+size_disp)->alloc == 0) {
-      printf("ALLOC == 0\n");
-      size_disp += (ptr+size_disp)->sizeblock + BH_SIZE;
-      goto out;
+    if (ptr == last  || ptr + size_disp == last ) {
+      limit_reached = 1;
+      printf("LAST\n");
     }
 
-	  if ((ptr+size_disp)->alloc == 1) {
-      printf("ALLOC == 1\n");
-		  ptr += (ptr+size_disp)->sizeblock + size_disp;
-		  size_disp = 0;
-	  }
-    out : ;
+    if (size_disp == size_asked){
+      printf("EQUAL\n");
+      best_fit = 1;
+      best_size = size_disp;
+      best_ptr = ptr;
+    }
+
+    if (size_disp > size_asked) {
+      printf("MORE\n");
+      fit = 1;
+      if (size_disp > best_size) {
+        best_ptr = ptr;
+        best_size = size_disp;
+      }
+      ptr += ptr->size;
+      //goto out;
+    }
+
+    if (size_disp < size_asked && !limit_reached) {
+      printf("LESS\n");
+      ptr += size_disp + BH_SIZE + (ptr+size_disp)->size;
+    }
+
+    //out : ;
+
+
+
   }
+
   printf("OUT\n");
 
-  if (size_disp != size_asked) {
-	  (ptr+size_asked+BH_SIZE)->alloc = 0;
-      printf("BSIZE : %i, SIZE_DISP : %ld, SIZE_ASKED : %ld\n", ptr->sizeblock, size_disp, size_asked);
-	  (ptr+size_asked+BH_SIZE)->sizeblock = size_disp - size_asked - BH_SIZE;
+  if (!fit) {
+    if (ptr+size_asked > limit) return NULL;
+    int mem = last->size;
+    ptr->alloc = 1;
+    ptr->size = size_asked;
+    last = ptr + BH_SIZE + size_asked;
+    last->alloc = 0;
+    last->size = mem - (size_asked - size_disp);
+    return ptr+BH_SIZE;
   }
 
-  ptr->alloc = 1;
-  ptr->sizeblock = size_asked;
-  return ptr+BH_SIZE;
+  printf("NON NULL\n");
+
+  if (best_size != size_asked) {
+      printf("RESIZE\n");
+	  (best_ptr+BH_SIZE+size_asked)->alloc = 0;
+    (best_ptr+BH_SIZE+size_asked)->size = best_size - size_asked - BH_SIZE;
+  }
+
+  best_ptr->alloc = 1;
+  best_ptr->size = size_asked;
+  return best_ptr+BH_SIZE;
 
 }
 
@@ -132,21 +128,24 @@ void free(void *ptr) {
 }
 
 int main(int argc, char const *argv[]) {
-  long *ptr = (long *) mymalloc (sizeof(long));
+  int *ptr = (int *) mymalloc (sizeof(int));
   *ptr = 8;
-  char *ptr2 = (char *) mymalloc (5*sizeof(char));
+  printf("Adresse 1 : %p, valeur (8): %i\n", ptr, *ptr );
+  char *ptr2 = (char *) mymalloc (1000);
   ptr2[0] = (char) 'B';
   ptr2[1] = (char) 'I';
   ptr2[2] = (char) 'T';
   ptr2[3] = (char) 'E';
   ptr2[4]= (char) '\0';
+  printf("Adresse 2 : %p, valeur (BITE): %s\n", ptr2, ptr2 );
   double *ptr3 = (double *) mymalloc (sizeof(double));
   *ptr3 = 13.5;
+  printf("Adresse 3 : %p, valeur (13.5): %lf\n", ptr3, *ptr3 );
   long *ptr4 = (long *) mymalloc (sizeof(long));
   *ptr4 = 22;
-  printf("Adresse 1 : %p, valeur (8): %ld\n", ptr, *ptr );
-  printf("Adresse 2 : %p, valeur (BITE): %s\n", ptr2, ptr2 );
-  printf("Adresse 3 : %p, valeur (13.5): %lf\n", ptr3, *ptr3 );
-  printf("Adresse 4 : %p, valeur (22): %ld\n", ptr4, *ptr4 );
+  printf("Adresse 1  : %p, valeur (8): %i\n", ptr, *ptr );
+  printf("Adresse 2  : %p, valeur (BITE): %s\n", ptr2, ptr2 );
+  printf("Adresse 3  : %p, valeur (13.5): %lf\n", ptr3, *ptr3 );
+  printf("Adresse 4  : %p, valeur (22): %ld\n", ptr4, *ptr4 );
   return 0;
 }
